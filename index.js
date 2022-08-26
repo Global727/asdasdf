8ab8021c0ddfe4b2fdf58370afaf9db3cf7dc7ff
Global727 <88992678+Global727@users.noreply.github.com>
2022-08-25 23:27:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -98,6 +98,6 @@ app.get('/account/all', function (req, res) {
     });
 });
 
-app.listen(process.env.PORT || 3000);;
+app.listen(process.env.PORT || 3000);
 
 console.log('Running on port: ' + port);
